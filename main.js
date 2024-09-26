bf924a66e356c0719e5869e3092faed41b99c6d4
anarab3l <ana.rabel@escola.pr.gov.br>
2024-09-26 11:38:39 -0300

    xcvbn

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ function soma(numero1,numero2){
 }
 console.log(soma(1,2));
 function multiplica(numero1,numero2){
-    return numero1%2;
+    return numero1%numero2;
 }
+console.log(multiplica(4%2));
