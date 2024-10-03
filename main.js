2ea56f672b4561b92ce0ee854f14dca4f9e70323
anarab3l <ana.rabel@escola.pr.gov.br>
2024-10-03 11:23:38 -0300

    ewre

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-alert ('ANA E MARI') 
-function soma(numero1,numero2){
-     return numero1+numero2;
-}
-console.log(soma(1,2));
-function areaQuadrado(lado){
-    return lado**2;
-}
-let numero=parseInt(prompt('numero'));
-console.log(areaQuadrado('numero'));
-let media=function(1,2,3**2){
-    return;
-}
-let numero1=parseInt(prompt('Qual o número 1?'));
-let numero2=parseInt(prompt('Qual o número 2?'));
-let numero3=parseInt(prompt('Qual o número 3?'));
-function media(x,y,z){
-    return (x,y,z)/3;
-}
-document.write (media(numero1,numero2,numero3));
+// alert ('ANA E MARI') 
+// function soma(numero1,numero2){
+//      return numero1+numero2;
+// }
+// console.log(soma(1,2));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// let numero=parseInt(prompt('numero'));
+// console.log(areaQuadrado('numero'));
+// let media=function(1,2,3**2){
+//     return;
+// }
+// let numero1=parseInt(prompt('Qual o número 1?'));
+// let numero2=parseInt(prompt('Qual o número 2?'));
+// let numero3=parseInt(prompt('Qual o número 3?'));
+// function media(x,y,z){
+//     return (x,y,z)/3;
+// }
+// document.write (media(numero1,numero2,numero3));
 
 let estados=['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF']
